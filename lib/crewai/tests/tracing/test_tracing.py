917c200fbba5f46082a2cc59be42837d7cd5a560
RabeyaMuna <rabeya.khatun.muna@g.bracu.ac.bd>
2026-04-16 18:35:51 -0400

    change-1

--- lib/crewai/tests/tracing/test_tracing.py
+++ lib/crewai/tests/tracing/test_tracing.py
@@ -322,7 +322,15 @@ class TestTraceListenerSetup:
     def test_trace_listener_setup_correctly_for_flow(self):
         """Test that trace listener is set up correctly when enabled"""
 
-        with patch.dict(os.environ, {"CREWAI_TRACING_ENABLED": "true"}):
+        with patch.dict(
+            os.environ,
+            {
+                "CREWAI_TRACING_ENABLED": "true",
+                "CREWAI_DISABLE_TELEMETRY": "false",
+                "CREWAI_DISABLE_TRACKING": "false",
+                "OTEL_SDK_DISABLED": "false",
+            },
+        ):
             class FlowExample(Flow):
                 @start()
                 def start(self):
